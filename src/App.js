ecf9058af75cccf22efe60335ed28a99fc31a0b2
Chadwick Strange <cstrangedk@users.noreply.github.com>
2017-11-04 12:51:24 -0700

    Completed basic grouping and card and name display using divs

--- src/App.js
+++ src/App.js
@@ -9,15 +9,45 @@ class App extends Component {
         'John Tuthill',
         'Kathy Belrose',
         'Jack Sanberg',
-        'Jill Stolen'
+        'Jill Stolen',
+        'John Doe',
+        'Niel Stevenson',
+        'Henry Gates',
+        'Luke Skywalker',
+        'Tina Fey',
+        'Irving Chernov',
+        'Vladimir Putin',
+        'Wang Ho',
+        'Frank Doe'
       ]
     };
   }
 
+  shuffle() {
+    let shuffledList = this.state.names.sort(function() { return 0.5 - Math.random() });
+    let groups = [];
+    for (let i = 0; i < shuffledList.length; i++) {
+      if (i % 4 === 0) {
+        groups.push([]);
+      }
+      groups[groups.length-1].push(shuffledList[i]);
+    }
+    return groups;
+  }
+
   render() {
+    let groups = this.shuffle();
+
+    let cards = groups.map((group, i) => <div className="Card" key={'card' + i}>
+        {group.map((name, i) => <span className="item" key={'item' + i}>{name}</span>)}
+      </div>);
+
     return (
       <div className="App">
-        {this.state.names.map((name) => <p>{name}</p>)}
+        <div className="NameList">
+         {this.state.names.map((name, i) => <p key={'main' + i}>{name}</p>)}
+        </div>
+        {cards}
       </div>
     );
   }
